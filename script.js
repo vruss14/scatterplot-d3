602e6417b8be45b2c3b1ed64f2d8adec125bb9a2
Valerie Russell <vruss14@gmail.com>
2022-01-20 18:22:09 -0500

    create axes & plot dots

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/maste
 function createScatterplot(data) {
     console.log(data);
 
-    // Set up the SVG canvas
+    // Set up the SVG canvas & append to the document
 
-    let width = 900;
+    let width = 800;
     let height = 600;
     let padding = 60;
 
@@ -20,6 +20,48 @@ function createScatterplot(data) {
     .attr('width', width)
     .attr('height', height)
 
+    // Set up the x axis and y axis
+    // x axis uses scaleLinear and is formatted to decimals (i.e. 2002 instead of 2,002)
+    // y axis uses scaleTime and formats the time from a JavaScript Date Object 
+    // in milliseconds to minutes and seconds
 
+    const xAxisScale = d3.scaleLinear()
+    .domain([d3.min(data, (d) => d.Year), d3.max(data, (d) => d.Year)])
+    .range([padding, width - padding])
+
+    const yAxisScale = d3.scaleTime()
+    .domain([d3.min(data, (d) => new Date (parseInt(d.Seconds) * 1000)), 
+        d3.max(data, (d) => new Date (parseInt(d.Seconds) * 1000))])
+    .range([padding, height - padding])
+
+    const xAxis = d3.axisBottom(xAxisScale).tickFormat(d3.format('d'))
+    const yAxis = d3.axisLeft(yAxisScale).tickFormat(d3.timeFormat('%M:%S'));
+
+    // Append the x and y axis to the scatterplot; adjust so that they line up nicely
+
+    svg.append('g')
+    .call(xAxis)
+    .attr('id', 'x-axis')
+    .attr("transform", "translate(0," + (height - padding) + ")")
+
+    svg.append("g")
+    .call(yAxis)
+    .attr('id', 'y-axis')
+    .attr("transform", "translate(" + padding + ",0)")
+
+    // Plot the dots on the scatterplot
+    // cx = where the dot should be placed on the x axis (based on year)
+    // cy = where the dot should be place on the y axis (based on time; needs to be Date object)
+
+    svg.selectAll('circle')
+    .data(data)
+    .enter()
+    .append('circle')
+    .attr('class', 'dot')
+    .attr('r', '5')
+    .attr('data-xvalue', (d) => d.Year)
+    .attr('data-yvalue', (d) => new Date (parseInt(d.Seconds) * 1000))
+    .attr('cx', (d) => xAxisScale(d.Year))
+    .attr('cy', (d) => yAxisScale(new Date (parseInt(d.Seconds) * 1000)))
 
 }
